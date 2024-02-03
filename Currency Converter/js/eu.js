b3d330b19406c67a8a708e4822d045f643ef54fd
Marko Plavsa <markoplavsa@outlook.com>
2024-02-03 17:21:13 +0100

    Update eu.js

--- Currency Converter/js/eu.js
+++ Currency Converter/js/eu.js
@@ -1,38 +1,23 @@
 function eurToAll() {
-
     setTimeout(eurToRsd,4);
     setTimeout(eurToUsDolar,4);
     setTimeout(eurToSwissFranc,4);
     setTimeout(eurToBritishPound,4);
-
-    //    setTimeout(refreshAll, 4000);
-
+  //setTimeout(refreshAll, 4000);
 }
-
 function eurToRsd() {
-
     var eurRsd = document.getElementById("euro").value * 119.21;
     document.getElementById("rsd").value = eurRsd.toFixed(2);
 }
-
-
 function eurToUsDolar() {
-
     var eurUsDolar = document.getElementById("euro").value * 1.19;
     document.getElementById("usDolar").value = eurUsDolar.toFixed(2);
 }
-
-
 function eurToSwissFranc() {
-
     var euChf = document.getElementById("euro").value * 1.16;
     document.getElementById("swissFranc").value = euChf.toFixed(2);
 }
-
 function eurToBritishPound() {
-
     var eurGbp = document.getElementById("euro").value * 0.88;
     document.getElementById("britPound").value = eurGbp.toFixed(2);
 }
-
-
